34a64c2b51c367ed6456d9c11fadc04e22c267c7
het07-patel <76025681+Het-07@users.noreply.github.com>
2025-03-22 15:36:57 -0300

    Add(C2): Refactor c2_app.py file

--- C2_app.py
+++ C2_app.py
@@ -8,17 +8,18 @@ PERSISTENT_STORAGE_PATH = "/het_PV_dir"
 @app.post("/compute")
 def compute(data: dict):
     if "file" not in data or "product" not in data:
-        return {"file": None, "error": "Invalid input"}
+        return {"file": None, "error": "Invalid JSON input."}
+    
     file_path = os.path.join(PERSISTENT_STORAGE_PATH, data["file"])
     if not os.path.exists(file_path):
-        return {"file": data["file"], "error": "File not found"}
+        return {"file": data["file"], "error": "File not found."}
 
     try:
         with open(file_path, 'r') as f:
             reader = csv.DictReader(f)
             if not {"product", "amount"}.issubset(reader.fieldnames):
-                raise ValueError("CSV format invalid")
+                return {"file": data["file"], "error": "Input file not in CSV format."}
             total = sum(int(row["amount"]) for row in reader if row["product"] == data["product"])
         return {"file": data["file"], "sum": total}
     except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+        return {"file": data["file"], "error": "Input file not in CSV format."}
