a9f60f5a5ba02e1a41aa3b15e741fb234f343996
het07-patel <76025681+Het-07@users.noreply.github.com>
2025-03-22 16:24:51 -0300

    Add(C2): Changes

--- C2_app.py
+++ C2_app.py
@@ -9,7 +9,7 @@ PERSISTENT_STORAGE_PATH = "/het_PV_dir"
 def compute(data: dict):
     if "file" not in data or "product" not in data:
         return {"file": None, "error": "Invalid JSON input."}
-    
+
     file_path = os.path.join(PERSISTENT_STORAGE_PATH, data["file"])
     if not os.path.exists(file_path):
         return {"file": data["file"], "error": "File not found."}
@@ -19,7 +19,9 @@ def compute(data: dict):
             reader = csv.DictReader(f)
             if not {"product", "amount"}.issubset(reader.fieldnames):
                 return {"file": data["file"], "error": "Input file not in CSV format."}
+
             total = sum(int(row["amount"]) for row in reader if row["product"] == data["product"])
-        return {"file": data["file"], "sum": total}
-    except Exception as e:
+            return {"file": data["file"], "sum": total} 
+    except:
         return {"file": data["file"], "error": "Input file not in CSV format."}
+
